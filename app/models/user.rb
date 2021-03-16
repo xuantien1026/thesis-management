1f1ddc8ea42e2bd993b816658b84137df25b28cc
xuantien1026 <xuantien1026@gmail.com>
2021-03-16 23:57:55 +0700

    Install rolify and add default student role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  rolify
+  after_create :assign_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def assign_default_role
+    add_role(:student) if roles.blank?
+  end
 end
