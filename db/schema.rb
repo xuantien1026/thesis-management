c3be43add833ebea72cdefd28eea4ddd2d823f11
xuantien1026 <xuantien1026@gmail.com>
2021-03-20 15:43:13 +0700

    add departments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_165103) do
+ActiveRecord::Schema.define(version: 2021_03_20_083757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "program"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+departments = [
+  { name: 'Department of Computer Science', program: 'CS' },
+  { name: 'Department of System and Networking', program: 'CS' },
+  { name: 'Department of Information System', program: 'CS' },
+  { name: 'Department of Software Engineering', program: 'CS' },
+  { name: 'Department of Computer Engineering', program: 'CE' },
+]
+
+departments.each { |d| Department.create d }
