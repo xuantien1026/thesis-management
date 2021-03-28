75be2fd84f1249fee7f09a0c145fc8a8faeeba1f
xuantien1026 <xuantien1026@gmail.com>
2021-03-28 12:55:25 +0700

    Fix dismissable alert bug

--- app/javascript/packs/coreui.js
+++ app/javascript/packs/coreui.js
@@ -1,3 +1,4 @@
 import * as coreui from '@coreui/coreui'
 
 document.querySelectorAll('[data-tooltip="true"]').forEach(el => { new coreui.Tooltip(el) })
+document.querySelectorAll('.alert').forEach(el => { new coreui.Alert(el) })
