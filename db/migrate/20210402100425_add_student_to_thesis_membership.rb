926fce894fcbb998526b170a11ee02f00e122544
xuantien1026 <xuantien1026@gmail.com>
2021-04-04 12:05:42 +0700

    Fix migration

--- db/migrate/20210402100425_add_student_to_thesis_membership.rb
+++ db/migrate/20210402100425_add_student_to_thesis_membership.rb
@@ -1,7 +1,5 @@
 class AddStudentToThesisMembership < ActiveRecord::Migration[6.1]
   def change
     add_reference :thesis_memberships, :student, foreign_key: { to_table: :users }, null: false
-    add_column :thesis_memberships, :midterm_result, :boolean
-    add_column :thesis_memberships, :midterm_note, :string
   end
 end
