9b3e0414c6924d57e30b39ffaae5b0cf97a69cee
syie <syie@syies-MacBook-Pro.local>
2021-04-18 23:20:02 +0700

    update lasted view lecturer featurer

--- app/controllers/lecturer_controller.rb
+++ app/controllers/lecturer_controller.rb
@@ -2,7 +2,8 @@
 class LecturerController < ApplicationController
   def index
     @departments = Department.where("faculty_id = ?", params[:faculty_id])
-    @lecturers = Lecturer.where("department_id = ?", @departments[0].id)
+    @departments_id = @departments.first ? @departments.first.id : -1
+    @lecturers = Lecturer.where("department_id = ?", @departments_id)
   end
   def department
     @lecturers = Lecturer.where("department_id = ?", params[:id])
--- app/javascript/packs/coreui.js
+++ app/javascript/packs/coreui.js
@@ -2,3 +2,4 @@ import * as coreui from '@coreui/coreui'
 
 document.querySelectorAll('[data-tooltip="true"]').forEach(el => { new coreui.Tooltip(el) })
 document.querySelectorAll('.alert').forEach(el => { new coreui.Alert(el) })
+document.querySelectorAll('[data-toggle="dropdown"]').forEach(el => { new coreui.Dropdown(el) })
