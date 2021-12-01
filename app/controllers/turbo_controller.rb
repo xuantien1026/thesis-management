74187142a5aca0c46b680d041e54aa359a0079f9
xuantien1026 <xuantien1026@gmail.com>
2021-12-01 22:58:18 +0700

    Thêm i18n cho devise & fix 1 số lỗi

--- app/controllers/turbo_controller.rb
+++ app/controllers/turbo_controller.rb
@@ -8,7 +8,7 @@ class TurboController < ApplicationController
       if get? # rubocop:disable Style/GuardClause
         raise e
       elsif has_errors? && default_action
-        render rendering_options.merge(format: :html, status: :unprocessable_entity)
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
         redirect_to navigation_location
       end
