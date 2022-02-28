0328cdfc2b758254c0d024cca020e7c956f3beb5
xuantien1026 <xuantien1026@gmail.com>
2022-02-28 07:30:41 +0700

    Change pdf layout

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
     @committee = DefenseCommittee.find(params[:defense_committee_id])
     respond_to do |format|
       format.pdf do
-        render pdf: 'bien_ban_hoi_dong_lvtn', margin: { top: 15, bottom: 15, left: 15, right: 15 },
+        render pdf: 'bien_ban_hoi_dong_lvtn', margin: { top: 10, bottom: 15, left: 15, right: 15 },
                show_as_html: params.key?('debug')
       end
     end
